2e932857daf2a4fb293e42ac63d523df472276a6
Olivier Lamy <olamy@apache.org>
2010-01-14 21:43:29 +0000

    [MOJO-1372]  If the parent of buildNumberPropertiesFileLocation doesn't exist, it should be created. Submitted by Peter Janes
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/buildnumber-maven-plugin@11652 52ab4f32-60fc-0310-b215-8acea882cd1b

--- src/main/java/org/codehaus/mojo/build/CreateMojo.java
+++ src/main/java/org/codehaus/mojo/build/CreateMojo.java
@@ -335,6 +335,10 @@ public class CreateMojo
                         {
                             try
                             {
+                                if (!propertiesFile.getParentFile().exists())
+                                {
+                                    propertiesFile.getParentFile().mkdirs();
+                                }
                                 propertiesFile.createNewFile();
                             }
                             catch ( IOException e )
