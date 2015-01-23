f5973df9c438a3f738c3ebee5bddbcda465dca5b
Dan Tran <dantran@gmail.com>
2015-01-23 23:57:00 +0000

    [MBUILDNUM-135] basic build info test
    git-svn-id: file:///home/tiste/MOJOHAUS-TO-GIT/SVN-MOJO-WIP/trunk/mojo/buildnumber-maven-plugin@20374 52ab4f32-60fc-0310-b215-8acea882cd1b

---
+++ src/test/java/org/codehaus/mojo/build/it/BuildInfoMojoTest.java
@@ -0,0 +1,50 @@
+package org.codehaus.mojo.build.it;
+
+import io.takari.maven.testing.TestResources;
+import io.takari.maven.testing.executor.MavenExecution;
+import io.takari.maven.testing.executor.MavenExecutionResult;
+import io.takari.maven.testing.executor.MavenRuntime;
+import io.takari.maven.testing.executor.MavenRuntime.MavenRuntimeBuilder;
+import io.takari.maven.testing.executor.MavenVersions;
+import io.takari.maven.testing.executor.junit.MavenJUnitTestRunner;
+
+import java.io.File;
+
+import org.junit.Assert;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith( MavenJUnitTestRunner.class )
+@MavenVersions( { "3.1.1" } )
+public class BuildInfoMojoTest
+{
+    @Rule
+    public final TestResources resources = new TestResources();
+
+    public final MavenRuntime maven;
+
+    public BuildInfoMojoTest( MavenRuntimeBuilder builder )
+        throws Exception
+    {
+        this.maven = builder.withCliOptions( "-B" ).build();
+    }
+
+    @Test
+    public void testBasicConfiguration()
+        throws Exception
+    {
+        File projDir = resources.getBasedir( "build-info-basic-it" );
+
+        MavenExecution mavenExec = maven.forProject( projDir );
+        MavenExecutionResult result = mavenExec.execute( "clean", "test" );
+        result.assertErrorFreeLog();
+
+        File testDir = result.getBasedir();
+        Assert.assertTrue( new File( testDir, "target/file1.properties" ).exists() );
+        Assert.assertTrue( new File( testDir, "target/xxx/file1.properties" ).exists() );
+        Assert.assertTrue( new File( testDir, "target/generated/buildinfo/build.properties" ).exists() );
+        Assert.assertTrue( new File( testDir, "target/classes/build.properties" ).exists() );
+
+    }
+}
