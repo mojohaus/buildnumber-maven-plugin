e5d3b40e498a6830aafe9069d8ff74c498aace4f
Tobias Gruetzmacher <tobias.gruetzmacher@inform-software.com>
2018-12-05 16:44:11 +0100

    Remove old invoker integration tests

--- src/it/basic-it-git/prebuild.groovy
+++
@@ -1,4 +0,0 @@
-File dotGitDir = new File(basedir, 'dotGitDir')
-assert dotGitDir.exists()
-assert dotGitDir.isDirectory()
-assert dotGitDir.renameTo(new File(basedir, '.git')) 
--- src/it/basic-it-svnjava/prebuild.groovy
+++
@@ -1,4 +0,0 @@
-File dotSvnDir = new File(basedir, 'dotSvnDir')
-assert dotSvnDir.exists()
-assert dotSvnDir.isDirectory()
-assert dotSvnDir.renameTo(new File(basedir, '.svn')) 
