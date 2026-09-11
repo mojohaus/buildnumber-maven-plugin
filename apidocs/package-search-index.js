e940238608480499bf4d674fca21096a00e2e83d
Sylwester Lachiewicz <slachiewicz@apache.org>
2026-09-12 00:18:44 +0200

    Site checkin for project Build Number Maven Plugin

--- apidocs/package-search-index.js
+++ apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"org.codehaus.mojo.build"},{"l":"org.codehaus.mojo.buildnumber_maven_plugin"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"org.codehaus.mojo.build"},{"l":"org.codehaus.mojo.buildnumber_maven_plugin"}]
