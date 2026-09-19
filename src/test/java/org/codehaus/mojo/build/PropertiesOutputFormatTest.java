3ef5acf37f25b4ddd89d306e40f768f3dc150fad
Sylwester Lachiewicz <slachiewicz@apache.org>
2026-09-19 23:51:25 +0200

    Drop Hamcrest from tests

--- src/test/java/org/codehaus/mojo/build/PropertiesOutputFormatTest.java
+++ src/test/java/org/codehaus/mojo/build/PropertiesOutputFormatTest.java
@@ -8,9 +8,8 @@ import java.util.Properties;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
 /**
@@ -45,7 +44,7 @@ public class PropertiesOutputFormatTest {
         Properties deserializedProperties = new Properties();
         deserializedProperties.load(new ByteArrayInputStream(serialized));
 
-        assertThat(deserializedProperties, is(properties));
+        assertEquals(properties, deserializedProperties);
     }
 
     private byte[] writeProperties() throws IOException {
